a503fde2f71e0cbdebd533ad93713f76a35c9bb7
Alex Comis <38458279+vezviitiko@users.noreply.github.com>
2023-12-21 11:08:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ def solution(pairs):
     for k,v in pairs.items():
         s += str(k)+ " = " + str(v) + ","
     return s[:-1]
+
+def solution(pairs):
+    return ','.join(f'{k} = {v}' for k,v in pairs.items())
